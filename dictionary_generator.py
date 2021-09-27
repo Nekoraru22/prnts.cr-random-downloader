26fde1f27b8980f0c648565b0621e4bdfe063fd8
Neko <57575090+Nekoraru22@users.noreply.github.com>
2021-09-27 14:42:01 +0200

    Overwrite instead of add the codes

--- dictionary_generator.py
+++ dictionary_generator.py
@@ -1,6 +1,6 @@
 import random, string
 
-txt = open('codes.txt', 'a')
+txt = open('codes.txt', 'w+')
 
 for i in range(5000):
     code = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(6)).lower()
