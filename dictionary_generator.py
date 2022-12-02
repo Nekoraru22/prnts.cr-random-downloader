b9c9318a71b0404e2a6f2fa43b2beb417e9beb6e
Neko <izangandiaruiz@gmail.com>
2022-12-02 22:45:58 +0100

    fixed errors

--- dictionary_generator.py
+++ dictionary_generator.py
@@ -1,10 +1,15 @@
 import random, string
+from colorama import init, Fore
 
+init()
 txt = open('codes.txt', 'w+')
+
+blue = Fore.LIGHTCYAN_EX
+white = Fore.LIGHTWHITE_EX
 
 for i in range(5000):
     code = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(6)).lower()
-    print(code)
+    print(f"{blue}[{i+1}] Generating code: {white}{code}")
     txt.write(f"{code}\n")
 
 txt.close()
